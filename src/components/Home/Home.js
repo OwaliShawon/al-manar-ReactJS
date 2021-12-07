6624d63bbaa24cfcd1b135dd460a3fbf2f0708eb
owalishawon <owali.shawon@gmail.com>
2021-12-07 17:24:49 +0600

    home section updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Carousel, Col, Container, Row, Button } from 'react-bootstrap';
 import "./Home.css"
+import bg from "../../assets/intig.png"
 
 const Home = () => {
 
     return (
-        <div>
-            <div className="bg-image"></div>
-
-            <div className="bg-text">
-                <p>SINCE 1990</p>
-                <h1>REAL SOLUTIONS!</h1>
-                <h2>Manufacturing Relationships Distributing Quality</h2>
-                <Button>Learn More</Button>
+        <section>
+            <div className="container">
+            <img src={bg} alt="Snow" className="image"/>
+                <div className="centered">
+                    <p>SINCE 1990</p>
+                    <h1>REAL SOLUTIONS!</h1>
+                    <h2>Manufacturing Relationships Distributing Quality</h2>
+                    <Button variant="primary" >Learn More</Button>
+                </div>
             </div>
-        </div>
+        </section>
     );
 };
 
